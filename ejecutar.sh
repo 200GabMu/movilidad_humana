a339c782a6e84de3ca9a9d202515c42333642267
200GabMu <menpaz.mh@gmail.com>
2026-09-04 16:39:14 -0500

    Delete ejecutar.sh

--- ejecutar.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Linux / Mac:  bash ejecutar.sh
-cd "$(dirname "$0")"
-if [ ! -x .venv/bin/python ]; then
-  python3 -m venv .venv && .venv/bin/python -m pip install --upgrade pip && .venv/bin/python -m pip install -r requirements.txt
-fi
-.venv/bin/python -m streamlit run app.py
